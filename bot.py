9dd63792863c564633ee095ae25ade1b1ef2d41f
hait38550-gif <hait38550@gmail.com>
2026-08-18 15:38:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,11 +58,11 @@ SMS_DB_FILE = "sms_db.json"
 
 # ==================== CẤU HÌNH SMM API ====================
 SMM_SERVICES_FIXED = {
-    "like_s2_clone": 6245,       # MC 6245 S2 Like clone nhanh
+    "like_s2_clone": 6683,       # MC 6683 Facebook like tây - Cực nhanh (1p /1k)
     "fol_clone_tay": 6444,       # MC 6444 Follow Tây + page nhanh
     "cmt_s2_sale": 6452,         # S2 Cmt Sale
     "page_s7_clone": 6450,       # MC 6450 S7 Like page + follow clone
-    "group_mem_s3": 6267,        # MC 6267 S3 Mem group
+    "group_mem_s3": 6451,        # S2 mem clone | Không bảo hành | max 40k | tốc độ cực nhanh
     "share_article": 6301,       # MC 6301 Share bài viết
     "view_video_reels": 6453,    # MC 6453 Facebook view - Video/Reels
     "mat_live_sv2": 6292,        # MC 6292 Sv2 mắt sale
